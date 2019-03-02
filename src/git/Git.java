0732d9e94454868e6c7f36b07f60af07e2acd814
mohamed <mohamed@DESKTOP-GO88S7T>
2019-03-02 12:10:20 +0200

    first commit

--- src/git/Git.java
+++ src/git/Git.java
@@ -17,6 +17,9 @@ public class Git {
     public static void main(String[] args) {
         // TODO code application logic here
     
+        for (int i = 0; i < 10; i++) {
+            System.out.println("fuck");
+        }
         System.out.println("new git project");
     
     }
